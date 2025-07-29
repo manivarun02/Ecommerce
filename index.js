012c046d6f86affa2365997bfb3d847b8c3e190c
= <=>
2025-07-29 11:58:54 +0530

    login/registe backend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import login from "./login.js"
-import register from "./register.js"
+import register, { bindRegister } from "./register.js"
 
 const root=document.getElementById('root')
 const allAnchors=document.querySelectorAll('a')
 
 const router={
-    "/login":login,
-    "/register":register
+    "/login":[login],
+    "/register":[register,bindRegister]
 }
 
 function handelClick(e){
@@ -14,7 +14,10 @@ function handelClick(e){
 //   console.log(e.target.pathname);
 let path=e.target.pathname
   history.pushState(null,"",`${path}`)
-root.innerHTML=router[path]()
+root.innerHTML=router[path][0]()
+if(router[path][1]){
+  router[path][1]()
+}
 }
 allAnchors.forEach((anchor)=>{
     anchor.addEventListener("click",handelClick)
@@ -46,7 +49,7 @@ const textArea=document.querySelector('textarea')
 function handelChnage(e){
 let {name,value,files}=e.target
 
-  if(name=="image"){
+  if(name=="profileImage"){
 value=files[0]
 const reader=new FileReader()
 reader.onload=function(){
@@ -76,8 +79,8 @@ function checkPasssword(e){
 function handelSubmit(e){
 e.preventDefault()
 // console.log(state);
-let {name,email,password,address,image}=state
-if(!name||!email||!password||!address||!image){
+let {name,email,password,address,profileImage}=state
+if(!name||!email||!password||!address||!profileImage){
   alert("All Feilds are mandatory")
   return
 }
@@ -90,22 +93,26 @@ if(password!=state["re-password"]){
 }
 // console.log(state);
 
-let payload={email,password,image,address,name}
-console.log(payload);
-
+let payload={email,password,profileImage,address,name}
+// console.log(payload);
+let formData=new FormData()
+for(let data in payload){
+  formData.append(data,payload[data])
+}
+(async()=>{
+ try {
+   let res= await fetch("http://localhost:5000/api/auth/register",{
+    method:"POST",
+    body:formData
+ })
+ let data=await res.json()
+ console.log(data);
+ 
+ } catch (error) {
+  console.log(error);
+  alert("Something Went wrong")
+ }
+})();
 
 }
 
-
-
-
-
-form.addEventListener('submit',handelSubmit)
-inputs.forEach(input=>{
-  input.addEventListener('change',handelChnage)
-})
-
-inputs.forEach(input=>{
-  input.addEventListener('input',checkPasssword)
-})
-textArea.addEventListener('change',handelChnage)
--- register.js
+++ register.js
@@ -1,7 +1,6 @@
 const register=()=>{
     return `
-  
-<div class="registerForm">
+  <div class="registerForm">
     <form action="">
         <div>
             <h1>Register</h1>
@@ -15,7 +14,7 @@ const register=()=>{
             <span><i class="fa-solid fa-envelope"></i></span>
         </div>
         <div>
-            <input type="password" name="password" placeholder="password">
+            <input type="password" name="password" placeholder="password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%&*])[A-Za-z\d!@#$%&*]{8,}$">
             <span><i class="fa-solid fa-key"></i></span>
         </div>
          <div>
@@ -31,7 +30,7 @@ const register=()=>{
             <span><i class="fa-solid fa-location-dot"></i></span>
         </div>
         <div>
-            <input type="file" accept=".png,.jpgx"  >
+            <input type="file" accept="image/*"  name="profileImage">
         </div>
 
         <div>
@@ -43,4 +42,14 @@ const register=()=>{
     `
 }
 
-export default register
+export let bindRegister=()=>{
+    form.addEventListener('submit',handelSubmit)
+inputs.forEach(input=>{
+  input.addEventListener('change',handelChnage)
+})
+
+inputs.forEach(input=>{
+  input.addEventListener('input',checkPasssword)
+})
+textArea.addEventListener('change',handelChnage)
+}
